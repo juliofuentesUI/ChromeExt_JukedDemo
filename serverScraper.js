47314bf90e0afab0264a650ef2effe59a806acdf
juliofuentes408 <juliofuentes@outlook.com>
2020-04-07 01:52:55 -0700

    Milestone! Successfully scraped all relevant data! Fixed scoping issues by moving extractEventInfo inside of traverseCalendar. calendarHandle.evaluate() runs in page context of browser. Does not see Node Context Scope.

--- serverScraper.js
+++ serverScraper.js
@@ -9,6 +9,8 @@ const RARLAB_XPATH = '/html/body/table/tbody/tr/td[2]/p[2]/img';
 
 const traverseCalendar = (calendar) => {
   //native calendar. Now traverse it with jQUERY or native javascript. your call. 
+  //DONT FORGET. THIS IS EXECUTIGN NOT IN NODE CONTEXT, BUT PAGE CONTEXT INSIDE BROWSER.
+  console.log('Function TraverseCalendar executing...');
   let eventDays = calendar.firstChild.children[1].children;
   let scrapedData = [];
   for (let day of eventDays) {
@@ -17,62 +19,65 @@ const traverseCalendar = (calendar) => {
 
     // console.log(day.children[1].firstChild.children);
   }
-  // console.log('eventDays is : ', eventDays);
+  console.log('scrapedData is', scrapedData);
   // remember at some point to check for list length to be 7 or 8. this will dictate a lot. 
   return null
+
+  function extractEventInfo(day) {
+    console.log('extractEventInfo is running! day value is : ', day);
+    let events = day.children[1].firstChild.children;
+    let nameOfDay = day.id;
+    let allEvents = [];
+    for(let event of events) {
+      if (event.children.length === 7) {
+        //Standard and most commonly seen calendar event.
+        let eventData = {};
+        eventData.nameOfDay = nameOfDay; //extracts "Live Now" or "Tomorrow"
+        eventData.timeTilStart = event.children[0].innerText; //extracts "2:00 am"
+        eventData.nameOfGame = event.children[1].innerText; //extracts "CSGO"
+        eventData.eventNameAndPrize = event.children[2].innerText; //extracts "NEX Play From Home Group B - Winners' match -$20,000"
+        eventData.eventLogo = event.children[2].querySelector('img').src; // extracts "https://img.abiosgaming.com/games/cs-square-logo.png"
+        eventData.team1Name = event.children[3].querySelector('.left').innerText; //extracts first teams name 'ZIGMA'
+        eventData.team2Name = event.children[3].querySelector('.right').innerText; //extracts second teams name 'TIGER'
+        eventData.team1Logo = event.children[3].querySelector('img').src; //extracts first teams logo url https://juked.gg/images/playerPlaceHolder.png
+        eventData.team1Logo = event.children[3].querySelectorAll('img')[1].src; //extracts second teams logo url https://juked.gg/images/playerPlaceHolder.png
+        allEvents.push(eventData);
+  
+      } else if (event.children.length === 6) {
+        //ONLY "LIVE NOW" events have this much markup
+        let eventData = {};
+        eventData.nameOfDay = nameOfDay; //extracts "Live Now" or "Tomorrow"
+        eventData.timeTilStart = 'Started'; // this one will be live, so it has viewer count instead.
+        eventData.liveViewerCount = event.children[0].innerText;
+        eventData.nameOfGame = event.children[1].innerText; //extracts "CSGO"
+        eventData.eventNameAndPrize = event.children[2].innerText; //extracts "NEX Play From Home Group B - Winners' match -$20,000"
+        eventData.eventLogo = event.children[2].querySelector('img').src; // extracts "https://img.abiosgaming.com/games/cs-square-logo.png"
+        eventData.team1Name = event.children[4].querySelector('.left').innerText; //extracts first teams name 'ZIGMA'
+        eventData.team2Name = event.children[4].querySelector('.right').innerText; //extracts second teams name 'TIGER'
+        eventData.team1Logo = event.children[4].querySelector('img').src; //extracts first teams logo url https://juked.gg/images/playerPlaceHolder.png
+        eventData.team1Logo = event.children[4].querySelectorAll('img')[1].src; //extracts second teams logo url https://juked.gg/images/playerPlaceHolder.png
+        allEvents.push(eventData);
+  
+      } else if (event.children.length === 8) {
+  
+        let eventData = {};
+        eventData.nameOfDay = nameOfDay; //extracts "Live Now" or "Tomorrow" or any other day
+        eventData.timeTilStart = `Starts in ${event.children[1].innerText}`; //extracts "41m"
+        eventData.nameOfGame = event.children[2].innerText; //extracts "CSGO"
+        eventData.eventNameAndPrize = event.children[3].innerText; //extracts "NEX Play From Home Group B - Winners' match -$20,000"
+        eventData.eventLogo = event.children[3].querySelector('img').src; // extracts "https://img.abiosgaming.com/games/cs-square-logo.png"
+        eventData.team1Name = event.children[4].querySelector('.left').innerText; //extracts first teams name 'ZIGMA'
+        eventData.team2Name = event.children[4].querySelector('.right').innerText; //extracts second teams name 'TIGER'
+        eventData.team1Logo = event.children[4].querySelector('img').src; //extracts first teams logo url https://juked.gg/images/playerPlaceHolder.png
+        eventData.team1Logo = event.children[4].querySelectorAll('img')[1].src; //extracts second teams logo url https://juked.gg/images/playerPlaceHolder.png
+        allEvents.push(eventData);
+  
+      }
+    }
+    return allEvents;
+  }
 };
 
-const extractEventInfo = (day) => {
-  let events = day.children[1].firstChild.children;
-  let nameOfDay = day.id;
-  let allEvents = [];
-  for(let event of events) {
-    if (event.children.length === 7) {
-      //Standard and most commonly seen calendar event.
-      let eventData = {};
-      eventData.nameOfDay = nameOfDay; //extracts "Live Now" or "Tomorrow"
-      eventData.timeTilStart = event.children[0].innerText; //extracts "2:00 am"
-      eventData.nameOfGame = event.children[1].innerText; //extracts "CSGO"
-      eventData.eventNameAndPrize = event.children[2].innerText; //extracts "NEX Play From Home Group B - Winners' match -$20,000"
-      eventData.eventLogo = event.children[2].querySelector('img').src; // extracts "https://img.abiosgaming.com/games/cs-square-logo.png"
-      eventData.team1Name = event.children[3].querySelector('.left').innerText; //extracts first teams name 'ZIGMA'
-      eventData.team2Name = event.children[3].querySelector('.right').innerText; //extracts second teams name 'TIGER'
-      eventData.team1Logo = event.children[3].querySelector('img').src; //extracts first teams logo url https://juked.gg/images/playerPlaceHolder.png
-      eventData.team1Logo = event.children[3].querySelectorAll('img')[1].src; //extracts second teams logo url https://juked.gg/images/playerPlaceHolder.png
-      allEvents.push(eventData);
-
-    } else if (event.children.length === 6) {
-      //ONLY "LIVE NOW" events have this much markup
-      let eventData = {};
-      eventData.nameOfDay = nameOfDay; //extracts "Live Now" or "Tomorrow"
-      eventData.timeTilStart = 'Started'; // this one will be live, so it has viewer count instead.
-      eventData.liveViewerCount = event.children[0].innerText;
-      eventData.nameOfGame = event.children[1].innerText; //extracts "CSGO"
-      eventData.eventNameAndPrize = event.children[2].innerText; //extracts "NEX Play From Home Group B - Winners' match -$20,000"
-      eventData.eventLogo = event.children[2].querySelector('img').src; // extracts "https://img.abiosgaming.com/games/cs-square-logo.png"
-      eventData.team1Name = event.children[4].querySelector('.left').innerText; //extracts first teams name 'ZIGMA'
-      eventData.team2Name = event.children[4].querySelector('.right').innerText; //extracts second teams name 'TIGER'
-      eventData.team1Logo = event.children[4].querySelector('img').src; //extracts first teams logo url https://juked.gg/images/playerPlaceHolder.png
-      eventData.team1Logo = event.children[4].querySelectorAll('img')[1].src; //extracts second teams logo url https://juked.gg/images/playerPlaceHolder.png
-      allEvents.push(eventData);
-
-    } else if (event.children.length === 8) {
-
-      let eventData = {};
-      eventData.nameOfDay = nameOfDay; //extracts "Live Now" or "Tomorrow" or any other day
-      eventData.timeTilStart = `Starts in ${event.children[1].innerText}`; //extracts "41m"
-      eventData.nameOfGame = event.children[2].innerText; //extracts "CSGO"
-      eventData.eventNameAndPrize = event.children[3].innerText; //extracts "NEX Play From Home Group B - Winners' match -$20,000"
-      eventData.eventLogo = event.children[3].querySelector('img').src; // extracts "https://img.abiosgaming.com/games/cs-square-logo.png"
-      eventData.team1Name = event.children[4].querySelector('.left').innerText; //extracts first teams name 'ZIGMA'
-      eventData.team2Name = event.children[4].querySelector('.right').innerText; //extracts second teams name 'TIGER'
-      eventData.team1Logo = event.children[4].querySelector('img').src; //extracts first teams logo url https://juked.gg/images/playerPlaceHolder.png
-      eventData.team1Logo = event.children[4].querySelectorAll('img')[1].src; //extracts second teams logo url https://juked.gg/images/playerPlaceHolder.png
-      allEvents.push(eventData);
-
-    }
-  }
-}
 
 
 async function scrapeData(url) {

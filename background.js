ce5e61f227a3f6fb2e4ea42cf03fa6b5dd9730f7
juliofuentes408 <juliofuentes@outlook.com>
2020-04-08 22:30:05 -0700

    Add listener for url changes. URL filters to associated domains only using object map

--- background.js
+++ background.js
@@ -1,32 +1,47 @@
-console.log('BACKGROUND.JS EXECUTED');
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//     if (request.fetchWarcraftDates) {
+//         fetch('https://juked.gg/wc3')
+//         .then(response => {
+//             console.log(response);
+//             return response.text();
+//         })
+//         .then(html => {
+//             console.log('Sending response back as html...');
+//             sendResponse(html);
+//         })
+//         return true; 
+//     //return true MEANS, at least the way chrome works.
+//     //that this will EVENTUALLY Send the repsonse back asynchronously.
+//     }
+// });
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.fetchWarcraftDates) {
-        fetch('https://juked.gg/wc3')
-        .then(response => {
-            console.log(response);
-            return response.text();
-        })
-        .then(html => {
-            console.log('Sending response back as html...');
-            sendResponse(html);
-        })
-        return true;
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//     console.log('changeInfo object', changeInfo);
+//     console.log('tab  object: ', tab);
+// });
+let associatedDomains = {
+    "https://www.hltv.org/": 'lol',
+    "https://www.dotabuff.com/": 'dota2'
+};
+
+chrome.webNavigation.onCompleted.addListener((tab) => {
+    //check to make sure its MAIN FRAME not subframe. mainframe is 0, rest are positive.
+    console.log('webNavigation onCompleted event fired');
+    if (tab.frameId === 0 && associatedDomains[tab.url]) {
+        console.log('This domain exist in our associated domains object');
     }
 
-    //return true MEANS, at least the way chrome works.
-    //that this will EVENTUALLY Send the repsonse back asynchronously.
+}, { url: [{ hostContains: 'hltv'}, {hostContains: 'dotabuff'}]});
+
+
+chrome.runtime.onSuspend.addListener(() => {
+    console.log('On Suspend event fired');
 });
 
 
-
-
-
-
-
-
-
-
+chrome.runtime.onSuspendCanceled.addListener(() => {
+    console.log('onSuspendCanceled fired');
+});
 
 
 
